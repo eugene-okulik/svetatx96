f160f7aa9d2b707fa1c26543d291f986f3c0e6fd
Svetlana Zubrilina <lana.zubrilina@gmail.com>
2024-02-28 10:52:18 -0600

    Add new homework_3й

---
+++ homework/sveta_zubrilina/lesson3/task3.py
@@ -0,0 +1,11 @@
+# Данные числа
+num1 = 10
+num2 = 20
+
+# Находим среднее арифметическое
+average_arithmetic = (num1 + num2) / 2
+print("Среднее арифметическое:", average_arithmetic)
+
+# Находим среднее геометрическое
+average_geometric = (num1 * num2) ** 0.5
+print("Среднее геометрическое:", average_geometric)
